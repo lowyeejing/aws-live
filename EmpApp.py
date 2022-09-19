d610b19d454dc5b7c505ff01dc20f9e073f3139d
Low Yee Jing <lowyeejing@gmail.com>
2022-09-19 21:11:37 +0800

    change route link

--- EmpApp.py
+++ EmpApp.py
@@ -32,11 +32,14 @@ def payroll():
 
 
 @app.route("/payroll/output", methods=['POST'])
-def about():
+def payrollOutput():
     return render_template('PayrollOutput.html')
 
+@app.route("/addemp", methods=['GET', 'POST'])
+def addEmp():
+    return render_template('AddEmp.html')
 
-@app.route("/addemp", methods=['POST'])
+@app.route("/addemp/output", methods=['POST'])
 def AddEmp():
     emp_id = request.form['emp_id']
     first_name = request.form['first_name']
@@ -86,13 +89,14 @@ def AddEmp():
     return render_template('AddEmpOutput.html', name=emp_name)
 
 
-if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=80, debug=True)
+@app.route("/attendance", methods=['GET', 'POST'])
+def att():
+    return render_template('Attendance.html')
 
-#Home
-@app.route("/home", methods=['GET', 'POST'])
-def home():
-    return render_template('Homepage.html')
+
+@app.route("/attendance/output", methods=['POST'])
+def attOutput():
+    return render_template('AttendanceOutput.html')
 
 #Leave
 @app.route("/leave", methods=['GET', 'POST'])
@@ -102,49 +106,8 @@ def leave():
 #LeaveOutput
 @app.route("/leave/output", methods=['POST'])
 def leaveOutput():
-    emp_id = request.form['emp_id']
-    first_name = request.form['first_name']
-    last_name = request.form['last_name']
-    pri_skill = request.form['pri_skill']
-    location = request.form['location']
-    emp_image_file = request.files['emp_image_file']
+    return render_template('LeaveOutput.html')  
 
-    insert_sql = "INSERT INTO employee VALUES (%s, %s, %s, %s, %s)"
-    cursor = db_conn.cursor()
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=80, debug=True)
 
-    if emp_image_file.filename == "":
-        return "Please select a file"
-
-    try:
-
-        cursor.execute(insert_sql, (emp_id, first_name, last_name, pri_skill, location))
-        db_conn.commit()
-        emp_name = "" + first_name + " " + last_name
-        # Uplaod image file in S3 #
-        emp_image_file_name_in_s3 = "emp-id-" + str(emp_id) + "_image_file"
-        s3 = boto3.resource('s3')
-
-        try:
-            print("Data inserted in MySQL RDS... uploading image to S3...")
-            s3.Bucket(custombucket).put_object(Key=emp_image_file_name_in_s3, Body=emp_image_file)
-            bucket_location = boto3.client('s3').get_bucket_location(Bucket=custombucket)
-            s3_location = (bucket_location['LocationConstraint'])
-
-            if s3_location is None:
-                s3_location = ''
-            else:
-                s3_location = '-' + s3_location
-
-            object_url = "https://s3{0}.amazonaws.com/{1}/{2}".format(
-                s3_location,
-                custombucket,
-                emp_image_file_name_in_s3)
-
-        except Exception as e:
-            return str(e)
-
-    finally:
-        cursor.close()
-
-    print("all modification done...")
-    return render_template('LeaveOutput.html', name=emp_name)
